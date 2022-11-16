de4f1dffaa1ea21cde9bdf4931ec04981bae5606
Josep Valls <alfredvays@gmail.com>
2022-11-16 16:15:54 +0100

    uso de Hooks

--- classes/Carrito.test.js
+++ classes/Carrito.test.js
@@ -1,35 +1,39 @@
 import Carrito from './Carrito.js';
 
 describe('Testing Carrito Class', () => {
+    let carrito
+    const nigiriItem = {
+        name: 'Nigiri',
+        price: 1.6
+    }
+    //Hook de jest para que instancoie un carrito para cada test
+    beforeEach(() => {
+        carrito = new Carrito()
+    });
+
     describe('getTotalItems', () => {
         it('Should return 0 items at initialization', () => {
-            const carrito = new Carrito();
             expect(carrito.getTotalItems()).toEqual(0);
         });
 
         it('Should return 1 item after adding 1 product', () => {
-            const carrito = new Carrito()
-            carrito.addItem({
-                name: 'Nigiri',
-                price: 1.6
-            });
+            carrito.addItem(nigiriItem);
             expect(carrito.getTotalItems()).toEqual(1);
         });
     });
 
     describe('addItem', () => {
+        
+        beforeEach(() => {
+            carrito.addItem(nigiriItem);
+        });
+
         it('Should have the item added in the Carrito', () => {
-            const carrito = new Carrito();
-            carrito.addItem({
-                name: 'Nigiri',
-                price: 1.6
-            });
-            expect(carrito.items).toContainEqual(
-                {
-                    name: 'Nigiri',
-                    price: 1.6
-                }
-            )
+            expect(carrito.items).toContainEqual(nigiriItem)
+        });
+
+        it('Should not contain an empty object when adding a nigiriItem', () => {
+            expect(carrito.items).not.toContainEqual({})
         });
     });
 });
